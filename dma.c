6e79deb682fc10d3c598778a89d7448667a7fca6
Don Kieu <donster@iastate.edu>
2018-11-02 13:59:25 -0500

    Update dma.c

--- dma.c
+++ dma.c
@@ -113,7 +113,7 @@ for (int j= 0; j<(led/40); j++){
 	memset(BlankArray,0,1);
 	uint32_t physDest = 0x7E20C018;
 	
-	unsigned int * srcData = virtSrcPage[0];	
+	unsigned int * srcData = (unsigned int *)virtSrcPage[0];	
 	for(int i = 0; i<(3*led);i++){
 		if(!(i%120) && i!=0){
 			printf("%d %d\n", i, i/120);
@@ -123,7 +123,7 @@ for (int j= 0; j<(led/40); j++){
 			cb_ptr->NEXTCONBK = (uint32_t)(virtTophys(led_cb[i/120]));
 			printf("Link to next block: %x %x\n",(uint32_t)led_cb[i/120], virtTophys(led_cb[i/150]));
 			cb_ptr = led_cb[i/120];
-			srcData = virtSrcPage[i];
+			srcData = (unsigned int *)virtSrcPage[i];
 			printf("Data Src to next block: %x %x\n", (uint32_t)srcData ,virtTophys(srcData));
 		} 
 		cb_ptr->TI = (5<<16)|(1<<6)| (1<<26)|(1<<1);
