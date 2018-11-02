602538761fb0f32c0743d9d782eff4722383ae6e
Don Kieu <donster@iastate.edu>
2018-11-02 13:20:04 -0500

    Update dma.c

--- dma.c
+++ dma.c
@@ -127,6 +127,7 @@ for (int j= 0; j<(led/40); j++){
 			printf("Link to next block: %x\n", virtTophys(led_cb[i/150]));
 			cb_ptr = led_cb[i/120];
 			srcData = srcArray[i/120];
+			printf("Data Src to next block: %x\n", virtTophys(srcData));
 		} 
 		cb_ptr->TI = (5<<16)|(1<<6)| (1<<26)|(1<<1);
 		cb_ptr->SOURCE_ADDR = (uint32_t)(virtTophys(srcArray[0]));
