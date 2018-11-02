4105f0b69bf0a13674e9f7f6730d15b9a6db2b46
Don Kieu <donster@iastate.edu>
2018-11-02 01:02:34 -0500

    Update dma.c

--- dma.c
+++ dma.c
@@ -63,7 +63,7 @@ int set_dma(){
 	
 	unsigned int * data_ptr = data;
 	for(int i = 0; i<led;i++){
-		*data_ptr = makeWord(0x00);
+		*data_ptr = makeWord(0xFF);
 		data_ptr++;
 		*data_ptr = makeWord(0x00);
 		data_ptr++;
