cf6505154b9b43ddb7b1fd38a4e2c8e6741486de
Don Kieu <donster@iastate.edu>
2018-11-02 11:26:35 -0500

    Update dma.c

--- dma.c
+++ dma.c
@@ -118,7 +118,7 @@ int set_dma(){
 	memcpy(srcArray, data, (led+1)*3*4);
 	
 	unsigned int * BlankArray = (unsigned int*)(virtBlankSrcPage);
-	memset(BlankArray,0,4096);
+	memset(BlankArray,data[led*3+1],1);
 	uint32_t physDest = 0x7E20C018;
 	
 		
