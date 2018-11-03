349cbc329e6953b0c650d2b47a4192fddfab18d5
Don Kieu <donster@iastate.edu>
2018-11-03 14:03:02 -0500

    Update LedDMA.c

--- LedDMA.c
+++ LedDMA.c
@@ -45,7 +45,7 @@ void makePulse(unsigned int head){
 		int pos = head-i;
 		//printf("%x |", (0xFF<<16)|(yellow<<8));
 		pos = pos>150 ? 150 : pos;
-		setColor(0xFF3200,pos);
+		setColor(0x110000,pos);
 	}
 
 }
