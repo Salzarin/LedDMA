019fb2fb3b72b963811e3794882d4e0b91519b59
Don Kieu <donster@iastate.edu>
2018-11-03 11:46:48 -0500

    Update dma.c

--- dma.c
+++ dma.c
@@ -65,6 +65,7 @@ void shutdown_dma(){
 void setColor(unsigned int color, int led_number){
 	volatile unsigned int* dma_channel = dma+0x500/4;
 	*(dma_channel) &=~0x1;
+	*(dma_channel) |=(31<<1)
 	unsigned int page_number = led_number/40;
 	LED_COLOR * srcData = (LED_COLOR *)virtSrcPage[page_number];
 	generateWave(srcData+led_number%40,color);
