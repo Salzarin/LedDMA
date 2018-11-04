9c297aaa3e4e746270f792ea6050beb889969c4f
Don Kieu <donster@iastate.edu>
2018-11-03 20:04:56 -0500

    Update color.h

--- color.h
+++ color.h
@@ -10,8 +10,8 @@ unsigned int HSLtoRGB(HSL hsl);
 unsigned int interpolateColor(HSL fromHSL, HSL toHSL, float increment, float step, HSL * hsl);
 
 void HSLset(HSL* dest, HSL* source){
-	dest.H = source.H;
-	dest.S = source.S;
-	dest.L = source.L;
+	dest->H = source->H;
+	dest->S = source->S;
+	dest->L = source->L;
 
 }
